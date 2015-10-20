d8638f97756fdb80aaabb475c9cc3030225c74ea
FedorVoloshyn <tikey.gm@gmail.com>
2015-10-20 19:06:53 +0300

    commit all

---
+++ Bezpeka_danyh_lr1_cripto/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bezpeka_danyh_lr1_cripto
+{
+    public partial class Form1 : Form
+    {
+        protected string toScript;
+        protected string fromScript;
+        public Form1()
+        {
+            InitializeComponent();
+            toScript = "";
+            fromScript = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string tmp1 = "";
+            toScript = textBox1.Text;
+            fromScript = "";
+
+            for (int i = 0; i < toScript.Length; i++)
+            {
+                tmp1 += toScript[toScript.Length - i - 1];
+                fromScript += (char)(tmp1[i] + 8);
+            }
+
+            textBox3.Text = "";
+            textBox3.Text = fromScript;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string result = "";
+            string tmp = "";
+
+            for (int i = 0; i < textBox2.Text.Length; i++)
+                tmp += (char)(textBox2.Text[i] - 8);
+
+            for (int i = 0; i < tmp.Length; i++)
+                result += tmp[tmp.Length - i - 1];
+
+            textBox4.Text = "";
+            textBox4.Text = result;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length != 0)
+                button1.Enabled = true;
+            else
+                button1.Enabled = false;
+            textBox3.Clear();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Length != 0)
+                button2.Enabled = true;
+            else
+                button2.Enabled = false;
+            textBox4.Clear();
+        }
+    }
+}
---
+++ Bezpeka_danyh_lr1_cripto/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Bezpeka_danyh_lr1_cripto")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bezpeka_danyh_lr1_cripto")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("ad7a9bdc-f7e9-469f-9c58-139f4c599d65")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
